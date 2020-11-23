f69dc40d4f7e5b1c13a19e2ab6e6ffb93778a4c2
msptechuk <61687790+msptechuk@users.noreply.github.com>
2020-11-23 15:50:38 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Hello, today is <?php echo date('l, F jS, Y'); ?></h1>
+    Hello, today is <?php echo date('l, F jS, Y'); ?>
+  </body>
+</html>
