06a748173024a6687cc0dbeb2a1b444870b7b54b
msptechuk <61687790+msptechuk@users.noreply.github.com>
2020-11-23 15:46:20 +0000

    Add files via upload

--- phpService.php
+++ phpService.php
@@ -9,6 +9,7 @@ print"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3
                $storeVal1 = $_GET["a1"];
                $storeVal2 = $_GET["a2"];
                $storeVal3 = $_GET["a3"];
+               $storeVal4 = $_GET["a4"];
             //Now you have the values of val1, val2, val3 stored in storeVal1, storeVal2, storeVal3 respectively.
             }
         print "</body>;
