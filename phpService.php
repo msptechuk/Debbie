71b2d01ac6b77bb004ec24984853ac96f32a55ba
msptechuk <61687790+msptechuk@users.noreply.github.com>
2020-11-23 11:40:23 +0000

    Add files via upload

---
+++ phpService.php
@@ -0,0 +1,15 @@
+<?php
+print"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">
+    <html xmlns="http://www.w3.org/1999/xhtml" xml:lang=\"en\" lang=\"en\">
+        <head>
+        </head>
+        <body>&";
+
+            if ($_SERVER['REQUEST_METHOD'] == "GET) {
+               $storeVal1 = $_GET["a1"];
+               $storeVal2 = $_GET["a2"];
+               $storeVal3 = $_GET["a3"];
+            //Now you have the values of val1, val2, val3 stored in storeVal1, storeVal2, storeVal3 respectively.
+            }
+        print "</body>;
+?>
